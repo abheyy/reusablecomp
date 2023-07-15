af6ad864d13481ce5c68a889e8d2604d75aaccdd
abheyy <iamabhaydhasmana@gmail.com>
2023-07-15 11:37:13 +0530

    a

---
+++ src/ProfileCard.js
@@ -0,0 +1,11 @@
+function ProfileCard(props)
+{
+    return(
+        <div>
+            <div>Title is {props.title}</div>
+            <div>Hanndle is {props.handle}</div>
+        </div>
+    );
+}
+
+export default ProfileCard;
